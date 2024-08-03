f2d6c5739ffc2cee0b49db52a42e5703acccd7a9
MayuriNa <159123557+MayuriNa@users.noreply.github.com>
2024-08-03 23:00:21 +0530

    Add files via upload

--- SquareCube.java
+++ SquareCube.java
@@ -1,16 +1,24 @@
+package BasicProgram;
 import java.util.Scanner;
 public class SquareCube {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("Enter number:");
-		double num = sc.nextDouble();
-		
-		double squareNumber = num*num;
-		double cubeNumber = num*num*num;
-		
-		System.out.println("Square of given number is :"+squareNumber);
-		System.out.println("Cube of a given number is :"+cubeNumber);
+
+	    public static void main(String[] args) {
+	       
+	        Scanner scanner = new Scanner(System.in);
+
+	        System.out.print("Enter a number: ");
+	        double number = scanner.nextDouble();
+	        		
+	        double power1 = Math.pow(number, 1);
+	        double power2 = Math.pow(number, 2);
+	        double power3 = Math.pow(number, 3);
+
+	        System.out.println("N^1 = " + power1);
+	        System.out.println("N^2 = " + power2);
+	        System.out.println("N^3 = " + power3);
+
+	    }
 	}
 
-}
+
+
